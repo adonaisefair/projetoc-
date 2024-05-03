3058a6ba9d9436dd1958f75a0df71be21335979b
adonaisefair <adonaisefair707@gmail.com>
2024-05-03 15:43:53 -0300

    Teste na API,criando....

--- projetoc#/Program.cs
+++ projetoc#/Program.cs
@@ -3,5 +3,7 @@ var app = builder.Build();
 //Minha local host http://localhost:5131
 
 app.MapGet("/", () => "Hello World,let's get it aaaa");
+app.MapGet("/", () => "Teste na api,criando....");
 
 app.Run();
+
