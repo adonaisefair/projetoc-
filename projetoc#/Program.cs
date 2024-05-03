bc9c15272396e4e14b99fea93a0c40960c0ee39c
adonaisefair <adonaisefair707@gmail.com>
2024-05-03 15:38:24 -0300

    Sua mensagem de commit aqui

--- projetoc#/Program.cs
+++ projetoc#/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 //Minha local host http://localhost:5131
 
-app.MapGet("/", () => "Hello World,let's get it");
+app.MapGet("/", () => "Hello World,let's get it aaaa");
 
 app.Run();
